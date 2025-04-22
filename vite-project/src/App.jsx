48e6216d1bf180313432376c2780450d98f8dc71
Sophya <sophyadamiazodev@gmail.com>
2025-04-22 19:13:38 -0300

    feat: Implementado o conceito de propriedades em componentes.

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,13 +3,14 @@ import { Post } from './Post'
 export function App() {
   return (
     <div>
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
+      <Post 
+        author=" Sophya Damiazo" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet tempore accusamus, excepturi suscipit, animi molestiae dolore cum itaque laudantium, sequi minus odit." 
+      />
+      <Post
+        author=" Gustavo Vieira"
+        content="Um novo post muito legal"
+      />
     </div>
   )
 }
-
