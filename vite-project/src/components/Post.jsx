7186d584f9a8d9f93255a17a4817f7dbed2c13c5
Sophya <sophyadamiazodev@gmail.com>
2025-04-26 20:59:51 -0300

    feat: Implementação do component Post.

---
+++ vite-project/src/components/Post.jsx
@@ -0,0 +1,25 @@
+import styles from './Post.module.css';
+
+export function Post() {
+    return(
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/devsophya.png"  />
+                    <div className={styles.authorInfo}>
+                        <strong>Sophya Damiazo</strong>
+                        <span>FullStack Developer</span>
+                    </div>
+                </div>
+                <time title="26 de Abril às 20:15h" datetime="2025-04-26 20:15:32"> Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href="">sophya.design/doctorcare</a></p>
+                <p> <a href="">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+        </article>
+    )
+}
