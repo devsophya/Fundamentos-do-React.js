63b652b17f2469b81bf5aae0da5577acc0b7a4cd
Sophya <sophyadamiazodev@gmail.com>
2025-04-28 02:59:15 -0300

    feat: Aplicando conceitos da programação declarativa.

--- vite-project/src/components/Comment.jsx
+++ vite-project/src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { HandHeart, HeartStraight, Trash } from '@phosphor-icons/react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment() {
+export function Comment({ content }) {
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/devsophya.png"  />
@@ -19,7 +19,7 @@ export function Comment() {
                         </button>
                     </header>
 
-                    <p>Muito bom Sophya, parabéns!! 👏👏</p>
+                    <p>{content}</p>
                 </div>
 
                 <footer>
