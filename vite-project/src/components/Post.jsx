910e1524e0e2ab5163c58bef5b00676b245f7940
Sophya <sophyadamiazodev@gmail.com>
2025-04-30 00:45:42 -0300

    feat: Implementação de validação no formulário.

--- vite-project/src/components/Post.jsx
+++ vite-project/src/components/Post.jsx
@@ -9,10 +9,12 @@ import { useState } from 'react';
 
 export function Post({ author, publishedAt, content }) {
     const [comments, setComments] = useState([
-        'Adorei o post'
+        'Adorei o post!'
     ])
 
     const [newCommentText, setNewCommentText] = useState('')
+
+    console.log(newCommentText);
 
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
@@ -24,19 +26,29 @@ export function Post({ author, publishedAt, content }) {
     })
 
     function handleCreateNewComment() {
-        event.preventDefault()
+        event.preventDefault();
 
         setComments([...comments, newCommentText]);
         setNewCommentText('');
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
     }
 
-    function deleteComment(comment) {
-        console.log(`Deletar comentário ${comment}`)
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório!');
     }
+
+    function deleteComment(commentToDelete) {
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentToDelete;
+        })
+        setComments(commentsWithoutDeletedOne);
+    }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
 
     return (
         <article className={styles.post}>
@@ -71,10 +83,14 @@ export function Post({ author, publishedAt, content }) {
                     placeholder="Deixe um comentário"
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
