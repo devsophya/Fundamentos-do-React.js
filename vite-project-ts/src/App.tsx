9d257940c2cc7cd24819543bf78185da98183b23
Sophya <sophyadamiazodev@gmail.com>
2025-05-02 10:01:54 -0300

    feat: Implementação do projeto em TS.

---
+++ vite-project-ts/src/App.tsx
@@ -0,0 +1,68 @@
+import { Header } from './components/Header.tsx';
+import { Sidebar } from './components/Sidebar.tsx';
+import { Post } from './components/Post.tsx'
+
+
+import styles from './App.module.css';
+
+import './global.css';
+
+// author: {avatar_url: "", name : "", role: ""}
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/devsophya.png',
+      name: 'Sophya Damiazo',
+      role: 'FullStack Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: '👉 sophya.design/doctorcare' },
+    ] as const,
+    publishedAt: new Date ('2025-04-20 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/GustavoVieiraa.png',
+      name: 'Gustavo Vieira',
+      role: 'FullStack Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: '👉 sophya.design/doctorcare' },
+    ] as const ,
+    publishedAt: new Date ('2025-04-27 21:00:00')
+  },
+];
+
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={[...post.content]}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
